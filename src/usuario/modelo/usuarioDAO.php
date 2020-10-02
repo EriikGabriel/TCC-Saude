a8fc293c7c7a86a0e1d7160ea79eced0b6e09293
EriikGabriel <erikgabriel.lins@hotmail.com>
2020-10-02 16:10:22 -0300

    Commit - Correção do Login (acesso do Admin)

--- src/usuario/modelo/usuarioDAO.php
+++ src/usuario/modelo/usuarioDAO.php
@@ -62,7 +62,9 @@ class UsuarioDao
             $arrayParam = array($id);
             $retorno = $this->crud->getSQLGeneric($sql, $arrayParam, TRUE);
 
-            if ($retorno > 0 && $check_password) {
+            if ($retorno[0]->nomeUsuario == "Administrador") {
+                if ($senha == $retorno[0]->senhaUsuario) return json_encode($retorno);
+            } else if ($retorno > 0 && $check_password) {
                 if (password_verify($senha, $retorno[0]->senhaUsuario)) return json_encode($retorno);
             } else {
                 if ($retorno > 0) echo json_encode($retorno);
